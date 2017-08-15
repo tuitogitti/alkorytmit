e81abec46cee78b8a03c9abf7ad65df1a359925e
tuitogitti <tuito@jamk.fi>
2017-08-15 12:15:56 +0300

    Add files via upload

---
+++ binarysearch.js
@@ -0,0 +1,32 @@
+/*
+ *binarySearch hakee taulukosta alkion (item) nimen
+ *perusteella sen indeksin. Se käyttää binary search
+ *-algoritmia jolla haku tehdään nopeudella 0(log n)
+ * Eli jos taulukossa on 8 alkiota, tarvitsee algoritmi
+ * suorittaa enintään log8 = 3 kertaa. 2 potenssiin 3 = 8.
+ */
+
+function binarySearch(arr, item) {
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low <= high) { // toistetaan kunnes etsintä kapenee yhteen alkioon
+        mid = Math.round((low + high) / 2);
+        guess = arr[mid];
+
+        if (item === guess) {
+            return mid; // lopetus
+        }
+        if (item < guess) { // item on puoliväliä alempana
+            high = mid - 1; // uusi high
+        } else { // item on puoliväliä ylempänä
+            low = mid + 1; // uusi low
+        }
+    }
+    return 'Not found!'; // Jos ei löydy, tapahtuu silmukan loputtua tämä
+}
+
+let myArray = [1, 2, 3, 4, 5, 6, 'huuhaa', 8];
+
+let index = binarySearch(myArray, 'huuhaa');
+console.log(index);
---
+++ quicksort.js
@@ -0,0 +1,39 @@
+/*
+ * quickSort lajittelee taulukon alkiot, jotka ovat numeroita,
+ * pienimmästä suurimpaan ja palauttaa lajitellun taulukon.
+ * Algoritmin nopeus on O(n*log n) eli jos taulukossa
+ * on 8 alkiota, tarvitsee algoritmi suorittaa enintään 8*3 = 24 kertaa.
+ * Eli sen pitäisi yleensä olla hieman nopeampi kuin selection sort.
+ * quickSort käyttää rekursiota joka on yleinen periaate algoritmeissa.
+ * Ensin etsitään mahdollisimman yksinkertainen 'base case'
+ * ja jos se ei toteudu, toistetaan 'recursive casea'.
+ */
+
+function quickSort(arr) {
+    let pivot; // taulukosta valitaan arvo johon muita verrataan
+    let smaller = []; // pienempi
+    let bigger = []; // suurempi
+
+    if (arr.length < 2) { // base case
+        return arr; // lopetus koska ei tarvitse sortata
+    } else { // recursive case
+        pivot = arr[0];
+        for (i = 1; i < arr.length; i++) {
+
+            if (arr[i] <= pivot) {
+                smaller.push(arr[i]);
+                //console.log('smaller: ' + smaller);
+            }
+            if (arr[i] > pivot) {
+                bigger.push(arr[i]);
+                //console.log('bigger: ' + bigger);
+            }
+        }
+        return ((quickSort(smaller)).concat(pivot)).concat(quickSort(bigger)); // rekursio
+    }
+}
+
+let myArray = [4, 3, 6, 5, 1, 2, 8, 7];
+
+let sortedArray = quickSort(myArray);
+console.log(sortedArray);
