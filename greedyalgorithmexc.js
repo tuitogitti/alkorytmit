ae427c98683e991e96e8c9d3371b3c5cc8e8239b
Tommi Tuikka <tuito@jamk.fi>
2018-08-25 10:56:07 +0300

    commentcommit

--- greedyalgorithmexc.js
+++ greedyalgorithmexc.js
@@ -1,13 +1,15 @@
 /*
- * Kauppamatkustajan ongelma on ehkäpä kuuluisin NP-täydellinen ongelma jonka
+ * Kauppamatkustajan ongelma on lyhimmän reitin valinta kun pitää käydä hyvin monissa
+ * eri kaupungeissa. Se on ehkäpä kuuluisin NP-täydellinen ongelma jonka
  * ratkaisunopeus pyrittäessä täydelliseen ratkaisuun on O(n!). Jos läpikäytäviä
  * kohteita on vaikkapa sata, nousee erilaisten ratkaisuvaihtoehtojen määrä
  * niin suureksi ettei kaikkien läpikäyminen ole mahdollista eikä järkevää.
  * Varsin hyvään tulokseen voidaan päästä yksinkertaisella "ahneella" algoritmilla.
  * Valitaan aloituskohde satunnaisesti ja käydään kaikki kohteet läpi siten että
- * mennään aina seuraavaan lähimpään kohteeseen. Näin etsitään nopein reitti
- * joka kulkee kaikkien kohteiden kautta.
- * Kokeile toteuttaa tämä algoritmi. Tee ensin Map-tietorakenne johon tallennat
- * muutaman kaupungin ja niiden etäisyydet muihin kaupunkeihin.
+ * mennään aina seuraavaan lähimpään kohteeseen. Näin löydetään järkevällä
+ * tarkkuudella lyhin reitti joka kulkee kaikkien kohteiden kautta.
+ * Kokeile toteuttaa tämä algoritmi. Tehdään ensin Map-tietorakenne johon tallennetaan
+ * muutama kaupunki ja niiden etäisyydet muihin kaupunkeihin.
  *
+ * https://github.com/tuitogitti/alkorytmit/blob/master/greedyalgorithmexc.js
  */
