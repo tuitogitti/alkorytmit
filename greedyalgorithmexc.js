e3bf7e802cd6043d99e8c3cc8c4ad6b409a3431b
Tommi Tuikka <tuito@jamk.fi>
2019-12-05 07:18:54 +0200

    excfix

--- greedyalgorithmexc.js
+++ greedyalgorithmexc.js
@@ -1,6 +1,6 @@
 /*
- * Kauppamatkustajan ongelma on lyhimmän reitin valinta kun pitää käydä hyvin monissa
- * eri kaupungeissa. Se on ehkäpä kuuluisin NP-täydellinen ongelma jonka
+ * Kauppamatkustajan ongelma on lyhimmän reitin valinta kun pitää käydä kaikissa
+ * kaupungeisssa joita on n kpl. Se on ehkäpä kuuluisin NP-täydellinen ongelma jonka
  * ratkaisunopeus pyrittäessä täydelliseen ratkaisuun on O(n!). Jos läpikäytäviä
  * kohteita on vaikkapa sata, nousee erilaisten ratkaisuvaihtoehtojen määrä
  * niin suureksi ettei kaikkien läpikäyminen ole mahdollista eikä järkevää.
@@ -8,26 +8,27 @@
  * Valitaan aloituskohde satunnaisesti ja käydään kaikki kohteet läpi siten että
  * mennään aina seuraavaan lähimpään kohteeseen. Näin löydetään järkevällä
  * tarkkuudella lyhin reitti joka kulkee kaikkien kohteiden kautta.
- * Kokeile toteuttaa tämä algoritmi. Tehdään ensin Map-tietorakenne johon tallennetaan
- * muutama kaupunki ja niiden etäisyydet muihin kaupunkeihin. Sitten tehdään funktio
- * jossa luodaan reitti taulukkoon.
+ * Kokeile toteuttaa tämä algoritmi.
  *
  * https://github.com/tuitogitti/alkorytmit/blob/master/greedyalgorithmexc.js
  */
 
 const cities = new Map();
 
-function makeRoute(cities) {
+function makeRoute(cities, start) {
     const route = [];
 
     /*
-    1) Valitaan satunnaisesti joku kaupunki cities mapista
+    1) Valitaan aloituskaupunki cities mapista
     2) Valitaan lyhin reitti seuraavaan kaupunkiin ja laitetaan valittu kaupunki route -taulukkoon
     3) Mennään cities-mapissa edellä valitun kaupungin kohdalle ja valitaan taas lyhin reitti
     seuraavaan kaupunkiin joka ei jo ole route -taulukossa.
     4) Tätä toistetaan niin kauan kunnes kaikki kaupungit on käyty läpi
+
+    Tämä ratkaisu toimii jos kaupunkeja on vain muutama, mutta jos kaupunkeja on vaikkapa sata
+    kappaletta, kasvaa cities -map turhan suureksi. Olisiko jokin toinen tietorakenne silloin parempi?
     */
-    // return route;
+    return route;
 }
 
 cities.set('Jkl', new Map([['Hel', 300], ['Tam', 150], ['Kuo', 130]]));
